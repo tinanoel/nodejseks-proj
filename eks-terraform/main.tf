e32084cf1f42946f627c88fc88ba70aee185bb7b
emmy06.nar <emmy06.nar@gmail.com>
2025-05-15 13:00:34 +0530

    updated ecr details

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "nodejs-eks-cluster"
-  cluster_version = "1.29"
+  cluster_version = "1.27"
   subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
