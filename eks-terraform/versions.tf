5ddcb9ecdf44d2176fb4a2aabedfff772ae482d3
emmy06.nar <emmy06.nar@gmail.com>
2025-05-15 15:28:35 +0530

    updated the main file

--- eks-terraform/versions.tf
+++ eks-terraform/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.50.0"
+      version = "5.50.0"
     }
   }
+  required_version = ">= 1.11.0"
 }
