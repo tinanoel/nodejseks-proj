4ce96c47829b304742025fe1c4aeb697c9883628
tinanoel <42848076+tinanoel@users.noreply.github.com>
2025-05-17 15:31:08 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -3,35 +3,28 @@ provider "aws" {
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 20.0.0"
-  cluster_name    = "nodejs-eks-cluster"
-  cluster_version = "1.29"
-  private_subnet_ids     = module.vpc.private_subnets
-  
-  
-  vpc_id          = module.vpc.name
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
 
-  #manage_aws_auth = true
-  eks_managed_node_groups = {
-    node_group = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_types   = ["t3.medium"]
-    }
+  cluster_name    = "example"
+  cluster_version = "1.31"
+
+  # Optional
+  cluster_endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
-}
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0.0"
-  name    = "nodejs-vpc"
-  cidr    = "10.0.0.0/16"
-  azs     = ["ap-south-1a", "ap-south-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  vpc_id     = "vpc-0aa3b5020d63663a0"
+  subnet_ids = ["subnet-0d3d8392a0ffd9fa7", "subnet-0474e8d2c87f43a8d"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
